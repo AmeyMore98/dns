9c57a90c6e0f1d2fd67859299ed76a8b452d9bc5
Amey More <amey8976@gmail.com>
2023-05-29 00:00:44 +0530

    WIP: Fix header packing

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const { buildQuery, headerToBytes } = require("./utils");
 const dgram = require("dgram");
 
 const query = buildQuery("www.example.com", 1);
+console.log("query", query);
 
-const socket = dgram.createSocket("udp4");
+// Create a UDP socket
+const sock = dgram.createSocket("udp4");
 
-// socket.bind(53);
-
-socket.send(query, 53, "8.8.8.8", (err) => {
-  if (err) console.error(err);
-  else console.log("Message sent");
+// Send our query to 8.8.8.8, port 53. Port 53 is the DNS port.
+sock.send(query, 0, query.length, 53, "8.8.8.8", (err) => {
+  if (err) {
+    console.error("Error sending query:", err);
+    sock.close();
+  }
+  console.log("Query sent");
 });
 
-socket.on("message", (msg, rinfo) => {
-  console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
+// Read the response. UDP DNS responses are usually less than 512 bytes,
+// so reading 1024 bytes is enough.
+sock.on("message", (response, rinfo) => {
+  console.log("Response:", response.toString("hex"));
+  sock.close();
+});
+
+// Handle errors
+sock.on("error", (err) => {
+  console.error("Socket error:", err);
+  sock.close();
 });
--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+const { randomBytes } = require("crypto");
 const DNSHeader = require("./DNSHeader");
 const DNSQuestion = require("./DNSQuestion");
 const struct = require("python-struct");
@@ -8,13 +9,13 @@ const struct = require("python-struct");
  * @returns
  */
 function headerToBytes(header) {
-  return struct.pack(">HHHHHH", ...header.getValues()).toString("binary");
+  return struct.pack("!HHHHHH", ...header.getValues()).toString("binary");
 }
 
 function questionToBytes(question) {
   return (
     question.name +
-    struct.pack(">HH", question.type, question.class_).toString("binary")
+    struct.pack("!HH", question.type, question.class_).toString("binary")
   );
 }
 
@@ -29,16 +30,26 @@ function encodeDnsName(domainName) {
 const TYPE_A = 1;
 const CLASS_IN = 1;
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 function buildQuery(domainName, recordType) {
   const name = encodeDnsName(domainName);
 
-  // Random number between 0 and 65535
-  const id = Math.floor(Math.random() * 65536);
+  // For some reason a max of 65535 gives incorrect results, randomly
+  const id = randomBytes(2).readUInt16BE();
+  // const id = 14274;
+  // const id = 0x1314;
 
-  RECURSION_DESIRED = 1 << 8;
+  const RECURSION_DESIRED = 1 << 8;
 
   const header = new DNSHeader(id, RECURSION_DESIRED, 1);
   const question = new DNSQuestion(name, recordType, CLASS_IN);
+
+  console.log("h", headerToBytes(header));
 
   return headerToBytes(header) + questionToBytes(question);
 }
